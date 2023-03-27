c56521ee43e482f672b8374e3712d9c908f04bee
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-27 10:01:48 +0200

    markup2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 import './fetchCountries.js'
 import fetchCountries from './fetchCountries.js'
 const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
-const listEl = document.querySelector("country-list");
-const divEl = document.querySelector("country-info");
+const listEl = document.querySelector(".country-list");
+const divEl = document.querySelector(".country-info");
 const inputEl = document.querySelector("#search-box");
 
-inputEl.addEventListener("input", debounce(onInputChange, DEBOUNCE_DELAY));
+inputEl.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
 
-
-function onInputChange(e) {
-    if (e.target.value === ''){
+function onSearch(ev) {
+    ev.preventDefault();
+    const countryName = ev.target.value.trim();
+    if (countryName === '') {
         divEl.innerHTML = "";
-        listEl.innerHTML = "";
+        listEl.innerHTML = ""
+    } else if (countryName.length > 10){
+        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+    } else if (countryName.length >= 2 && countryName.length <= 10){
+        
     }
-     const countryName = (e.target.value).trim;
+    
      fetchCountries(countryName)
-    .then((contriesObj) => {console.log(contriesObj)})}
+    .then(countriesObj => {console.log(countriesObj)})}
+   
