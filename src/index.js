abda12c7a3b52a290713d239a366f2ad13b5447e
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-28 14:50:00 +0200

    6

--- src/index.js
+++ src/index.js
@@ -1,46 +1,54 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import './fetchCountries.js'
-import fetchCountries from './fetchCountries.js'
+import './fetchCountries.js';
+import fetchCountries from './fetchCountries.js';
 const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
-const listEl = document.querySelector(".country-list");
-const divEl = document.querySelector(".country-info");
-const inputEl = document.querySelector("#search-box");
+const listEl = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info');
+const inputEl = document.querySelector('#search-box');
 
-inputEl.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(ev) {
-    ev.preventDefault();
-    const countryName = ev.target.value.trim();
-        if (countryName === '') {
-        divEl.innerHTML = "";
-        listEl.innerHTML = ""
-    }  fetchCountries(countryName).then((countriesObj) => {
-                        let backup = '';
-                        let card = '';
-                        if (countriesObj.length > 10){
-                            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-                        } else if (countriesObj.length >= 2 && countriesObj.length <= 10){
-                             
-                            countriesObj.forEach((countryItem) => {
-            backup += `<li class="country__list"><img  class="flag" src="${countryItem.flags.svg}" alt="${countryItem.name.common}" /><p>${countryItem.name.official}</p></li>`;
-        }); 
-        listEl.innerHTML = backup;}
-        else{ countriesObj.map((country) => {
-            listEl.innerHTML = "";
-                 card += `<ul>
-                <li><h1><img  class="flag" src="${country.flags.svg}" alt="${country.name.common}" /> ${country.name.official}</h1</li>
+  ev.preventDefault();
+  const countryName = ev.target.value.trim();
+  if (countryName === '') {
+    divEl.innerHTML = '';
+    listEl.innerHTML = '';
+    return;
+  }
+  fetchCountries(countryName).then(countriesObj => {
+    let backup = '';
+    let card = '';
+    if (countriesObj.length > 10) {
+      Notiflix.Notify.warning(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    } else if (countriesObj.length >= 2 && countriesObj.length <= 10) {
+        divEl.innerHTML = '';
+      countriesObj.forEach(countryItem => {
+        backup += `<li class="country__list"><img  class="flag" src="${countryItem.flags.svg}" alt="${countryItem.name.common}" /><p>${countryItem.name.official}</p></li>`;
+      });
+      listEl.innerHTML = backup;
+      
+    } else {
+      countriesObj.map(country => {
+        listEl.innerHTML = '';
+        card += `<ul>
+                <li><h1><img  class="flag" src="${country.flags.svg}" alt="${
+          country.name.common
+        }" /> ${country.name.official}</h1</li>
                 <li><p>Capital: ${country.capital}</p></li>
                 <li><p>Population: ${country.population}</p></li>
-                <li><p>Languages: ${Object.values(country.languages).join(", ")}</p></li>
+                <li><p>Languages: ${Object.values(country.languages).join(
+                  ', '
+                )}</p></li>
                 </ul>`;
-            }); 
-            divEl.innerHTML = card;}
-        }
-                    )}
-                    
-    
-
+      });
+      divEl.innerHTML = card;
+    }
+  });
+}
